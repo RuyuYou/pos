180167686a06ec1b4ec01a7c10309eed9466f3a8
Ruyu You <727809166@qq.com>
2016-07-18 21:07:20 +0800

    finish function of buildCartItems

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,3 +1,55 @@
-'use strict';
+function buildCartItems(inputs, allItems) {
+  let cartItems = [];
+  let items = getItemsCount(inputs);
+  items.forEach((item)=> {
+    let allItem = allItems.find((allItem)=> {
+      return item.barcode === allItem.barcode;
+    });
+    if (allItem) {
+      cartItems.push({item: allItem, count: item.count});
+    }
+  });
+  return cartItems;
+}
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+getItemsCount = ( (inputs)=> {
+  let items = [];
+  inputs.forEach((input)=> {
+    let inputArray = input.split('-');
+    let barcode = inputArray[0];
+    let count = parseFloat(inputArray[1] || 1);
+    let item = items.find((item)=>{
+      return item.barcode === barcode;
+    });
+    if (item) {
+      item.count ++;
+    }
+    else {
+      items.push({barcode: barcode, count: count});
+    }
+  });
+  return items;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
