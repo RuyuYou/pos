3c9876eed5010e59c40d2155554f88152ee3e352
Ruyu You <727809166@qq.com>
2016-07-19 12:08:16 +0800

    finish function of buildSubTotal and saved

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,3 +1,35 @@
+'use strict';
+
+function printReceipt(inputs, allItems, promotions) {
+  let cartItems = buildCartItems(inputs, allItems);
+  let printCartItems = buildSubTotalAndSaved(cartItems, promotions);
+}
+
+function buildSubTotalAndSaved(cartItems, promotions) {
+  let receiveCartItems = [];
+  let freeBarcode = promotions[0].barcodes;
+  for (let cartItem of cartItems) {
+    let promotion = freeBarcode.find((barcode)=> {
+      return barcode === cartItem.item.barcode;
+    });
+    if (promotion) {
+      let saveCount = Math.floor(cartItem.count / 3);
+      let count = cartItem.count -saveCount;
+      let saved = saveCount * cartItem.item.price;
+      let subTotal = count * cartItem.item.price;
+      receiveCartItems.push({cartItem, subTotal, saved});
+    }
+    else {
+      let count = cartItem.count;
+      let saved =0;
+      let subTotal = cartItem.item.price * count;
+      receiveCartItems.push({cartItem, subTotal, saved});
+    }
+  }
+  return receiveCartItems;
+}
+
+
 function buildCartItems(inputs, allItems) {
   let cartItems = [];
   let items = getItemsCount(inputs);
@@ -12,17 +44,17 @@ function buildCartItems(inputs, allItems) {
   return cartItems;
 }
 
-getItemsCount = ( (inputs)=> {
+let getItemsCount = ( (inputs)=> {
   let items = [];
   inputs.forEach((input)=> {
     let inputArray = input.split('-');
     let barcode = inputArray[0];
     let count = parseFloat(inputArray[1] || 1);
-    let item = items.find((item)=>{
+    let item = items.find((item)=> {
       return item.barcode === barcode;
     });
     if (item) {
-      item.count ++;
+      item.count++;
     }
     else {
       items.push({barcode: barcode, count: count});
